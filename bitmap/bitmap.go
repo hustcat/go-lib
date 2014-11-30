adc134259afc368084198d468b5a884351c9909e
Ye Yin <eyniy@qq.com>
2014-11-30 11:19:52 +0800

    add some notes

--- bitmap/bitmap.go
+++ bitmap/bitmap.go
@@ -3,13 +3,22 @@
 // license that can be found in the LICENSE file.
 // Author：Ye Yin<hustcat@gmail.com>
 
-// NUMA CPU bitmap, used to affinity progress to some CPU
+// NUMA CPU bitmap, used to affinity progress to some CPU.
+// CPU must be hypethreaded, and CPU no look like as follows:
+// [node0, node1, ... , node0, node1, ...]
+// For example:
+// node0: [0,1,2,3,4,5,12,13,14,15,16,17]
+// node1: [6,7,8,9,10,11,18,19,20,21,22,23]
+
 package bitmap
 
 import "fmt"
 
+//24 cores
 const defaultSize = 24
 const maxSize = 1024
+
+//two nodes
 const defaultNodeNum = 2
 
 type NumaBitmap struct {
@@ -122,6 +131,9 @@ func (b *NumaBitmap) Get1BitOffsNuma(nodeNum uint) ([][]uint, error) {
 			//exlude hyperthread
 			if offset >= cpu {
 				tmp = offset - cpu
+				for tmp >= cpu {
+					tmp = offset - cpu
+				}
 			} else {
 				tmp = offset
 			}
--- bitmap/bitmap_test.go
+++ bitmap/bitmap_test.go
@@ -67,3 +67,52 @@ func TestGet1BitOffsNuma(t *testing.T) {
 		t.Errorf("expected:%v, actual:%v", a, b)
 	}
 }
+
+func TestGet1BitOffsNuma4(t *testing.T) {
+	bitmap := NewNumaBitmapSize(48, 4)
+
+	//node 0
+	bitmap.SetBit(0, 1)
+	bitmap.SetBit(5, 1)
+
+	//node 1
+	bitmap.SetBit(6, 1)
+	bitmap.SetBit(11, 1)
+
+	//node 2
+	bitmap.SetBit(12, 1)
+	bitmap.SetBit(17, 1)
+
+	//node 3
+	bitmap.SetBit(18, 1)
+	bitmap.SetBit(23, 1)
+
+	//node 0
+	bitmap.SetBit(24, 1)
+	bitmap.SetBit(29, 1)
+
+	//node 1
+	bitmap.SetBit(30, 1)
+	bitmap.SetBit(35, 1)
+
+	//node 2
+	bitmap.SetBit(36, 1)
+	bitmap.SetBit(41, 1)
+
+	//node 3
+	bitmap.SetBit(42, 1)
+	bitmap.SetBit(47, 1)
+
+	actual, _ := bitmap.Get1BitOffsNuma(4)
+	expected := [][]uint{
+		[]uint{0, 5, 24, 29},
+		[]uint{6, 11, 30, 35},
+		[]uint{12, 17, 36, 41},
+		[]uint{18, 23, 42, 47},
+	}
+	a := fmt.Sprintf("%v", expected)
+	b := fmt.Sprintf("%v", actual)
+	if a != b {
+		t.Errorf("expected:%v, actual:%v", a, b)
+	}
+}
